010675a67704be3e7c0bffc32f2a850fd1616e50
Matthew Costa <ucosty@gmail.com>
2012-10-20 03:22:00 +1100

    Editing products works now

--- app/controllers/products.php
+++ app/controllers/products.php
@@ -29,6 +29,30 @@
 			Templates::ActionTemplate(array("products" => $products));
 		}
 
+		public function show()
+		{
+			$product = Product::find(Params::id());
+
+			$product_vars['name'] = $product->name;
+			$product_vars['cost'] = $product->cost / 100;
+			$product_vars['image'] = $product->image;
+
+			Templates::ActionTemplate(array("product" => $product_vars));
+		}
+
+		public function update()
+		{
+			if($_SERVER['REQUEST_METHOD'] == "POST")
+			{
+				$product = Product::find($_POST['id']);
+				$product->name = $_POST['Name'];
+				$product->cost = $_POST['Cost'] * 100;
+				$product->image = $_POST['Image'];
+
+				$product->save();
+			}
+		}
+
 		public function delete()
 		{
 			if($_SERVER['REQUEST_METHOD'] == "POST")
